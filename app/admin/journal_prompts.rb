881bf2f660061e625502dcf27b20ef1a5a527337
Raghu Betina <raghu@firstdraft.com>
2019-06-01 17:31:27 +0000

    Whitelist columns in JournalPrompt for ActiveAdmin

--- app/admin/journal_prompts.rb
+++ app/admin/journal_prompts.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register JournalPrompt do
+
+ permit_params :prompt_text, :day, :journal_method_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
