3de9f8077181c4610ea417cfaad89f8c4de0a207
Raghu Betina <raghu@firstdraft.com>
2019-06-01 17:31:18 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "journal_prompts#index"
   # Routes for the Journal method resource:
 
